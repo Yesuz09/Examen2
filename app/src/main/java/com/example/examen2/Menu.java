d71160ec4a8f91df1b064de78a3d8c22605dbdbf
Yesuz <tmp_jcamacho@accitesz.com>
2023-11-01 11:43:28 -0600

    Comit inicial.2

--- app/src/main/java/com/example/examen2/Menu.java
+++ app/src/main/java/com/example/examen2/Menu.java
@@ -3,20 +3,23 @@ package com.example.examen2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.content.Intent;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class Menu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button btMainLink1 = findViewById(R.id.bt_LoginN);
+        setContentView(R.layout.activity_menu);
+        Button btMainLink1 = findViewById(R.id.BtChats);
         btMainLink1.setOnClickListener(v -> {
             Intent intent = new Intent (v.getContext(),activity_menu.class);
             startActivity(intent);
 
+            Button btMainLink2 = findViewById(R.id.BtTareas);
+            btMainLink1.setOnClickListener(v -> {
+                Intent intent = new Intent (v.getContext(), activity_menu_tareas.class);
+                startActivity(intent);
 
         });
     }
